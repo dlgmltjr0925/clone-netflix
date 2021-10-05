d7b9c9f8a9bd78b138dd252fd655aac78613ef37
fine <dlgmltjr0925@gmail.com>
2021-10-05 22:38:16 +0900

    feature: Add react router library

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+import reportWebVitals from "./reportWebVitals";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
